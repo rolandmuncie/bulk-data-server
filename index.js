b25acbf5d6bbaea25a27c3acad41675c417bf9e1
rolandmuncie <rolandmuncie@gmail.com>
2018-09-29 11:42:03 -0400

    lose the https redirect

--- index.js
+++ index.js
@@ -17,15 +17,7 @@ if (process.env.NODE_ENV != "test") {
     app.use(morgan("combined"));
 }
 
-// HTTP to HTTPS redirect (this is Heroku-specific!)
-app.use((req, res, next) => {
-    let proto = req.headers["x-forwarded-proto"];
-    let host  = req.headers.host;
-    if (proto && (`${proto}://${host}` !== config.baseUrl)) { 
-        return res.redirect(301, config.baseUrl + req.url);
-    }
-    next();
-});
+
 
 // backend services authorization
 app.post("/auth/token", bodyParser.urlencoded({ extended: false }), tokenHandler);
