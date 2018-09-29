f81a27f38284dbf99c9932440323dce8f1b3d022
rolandmuncie <rolandmuncie@gmail.com>
2018-09-29 11:53:35 -0400

    https

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const PORT = process.env.PORT || (process.env.NODE_ENV == "test" ? 9444 : 9443);
 module.exports = {
 
-    baseUrl: "http://r1-bulkfhir.paas-poc.am.lilly.com",
+    baseUrl: "https://r1-bulkfhir.paas-poc.am.lilly.com",
     port: PORT,
 
     throttle: 1,
